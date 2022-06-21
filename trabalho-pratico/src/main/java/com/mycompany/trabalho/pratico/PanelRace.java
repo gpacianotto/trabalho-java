deb2530a615517662a6978df41e60010d588f342
Kadu-60 <carlos.gm@outlook.com.br>
2022-06-21 20:39:33 -0300

    ajuste

--- trabalho-pratico/src/main/java/com/mycompany/trabalho/pratico/PanelRace.java
+++ trabalho-pratico/src/main/java/com/mycompany/trabalho/pratico/PanelRace.java
@@ -16,51 +16,72 @@ import javax.swing.JPanel;
  * @author guilh
  */
 public class PanelRace extends JPanel {
-    
-    
-    private ArrayList<DisplayModule> displays;
-    private int locationX;
-    private int locationY;
-    private final int distanceBetweenDisplays = 15;
-    private final int displayHeight = 30;
-    Race carRace;
 
-    public PanelRace(int numberOfCars, int locationX, int locationY) {
-        Config config = Config.getInstance();
-        this.displays = new ArrayList<>();
-        this.locationX = locationX;
-        this.locationY = locationY;
-        
-        for(int i = 0; i < numberOfCars; i++)
-        {
-            this.displays.add(new DisplayModule(this.locationX, this.locationY + (i + 1)* (displayHeight + distanceBetweenDisplays), i+1));
-        }
-        
-        System.out.println("Quantidade de Carros: "+ config.getNumberOfCars());
-        System.out.println("Quantidade de Voltas: "+ config.getNumberOfLaps());
-        System.out.println("Probabilidade de Quebrar: "+ config.getProbBreak() + "%");
-        System.out.println("Probabilidade de Abastecer: "+ config.getProbFuel() + "%");
-        
-        carRace = new Race(displays);
-        
-        
-    }
-    
-    @Override
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-        
-        
-        //g.fillRect(posiçãoX, posiçãoY
-        for(DisplayModule display: this.displays)
-        {
-           display.paintDisplay(g); 
-        }
-        
+	private ArrayList<DisplayModule> displays;
+	private int locationX;
+	private int locationY;
+	private final int distanceBetweenDisplays = 15;
+	private final int displayHeight = 30;
+	private ArrayList<DisplayModule> winnersList;
+
+	public PanelRace(int numberOfCars, int locationX, int locationY) {
+		Config config = Config.getInstance();
+		this.displays = new ArrayList<>();
+		this.locationX = locationX;
+		this.locationY = locationY;
+
+		for (int i = 0; i < numberOfCars; i++) {
+			this.displays.add(new DisplayModule(this.locationX,
+					this.locationY + (i + 1) * (displayHeight + distanceBetweenDisplays), i + 1));
+		}
+
+		System.out.println("Quantidade de Carros: " + config.getNumberOfCars());
+		System.out.println("Quantidade de Voltas: " + config.getNumberOfLaps());
+		System.out.println("Probabilidade de Quebrar: " + config.getProbBreak() + "%");
+		System.out.println("Probabilidade de Abastecer: " + config.getProbFuel() + "%");
+
+		this.winnersList = new ArrayList<>();
+
+		while (winnersList.size() < config.getNumberOfCars()) {
+			for (DisplayModule displayCar : displays) {
+
+				if (displayCar.getCarLocation() < 1000) {
+					displayCar.carMoves();
+					if (displayCar.getCarLocation() > 999) {
+						if (displayCar.getCarLaps() >= config.getNumberOfLaps()) {
+							this.winnersList.add(displayCar);
+						} else {
+							displayCar.addCarLaps();
+							System.out.println("Carro N" + displayCar.getCarNumber() + " completou a sua "
+									+ displayCar.getCarLaps() + " volta!");
+						}
+
+					}
+				}
+			}
+		}
+		for (DisplayModule podium : winnersList) {
+
+			System.out.println("Ganhador carro N" + podium.getCarNumber());
+
+		}
+
+		
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		// g.fillRect(posiçãoX, posiçãoY
+		for (DisplayModule display : this.displays) {
+			display.paintDisplay(g);
+		}
+
 //        for(Point point: points)
 //        {
 //            g.fillOval(point.x, point.y, 4, 4);
 //        }
-    }
+	}
 }
